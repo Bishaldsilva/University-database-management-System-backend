a9ffff43d282dcc5b0abdd8481a57ff9fd069e0d
Bishal <bishald288@gmail.com>
2024-05-02 10:03:19 +0530

    Get-Routine-by-day and get-routine-by-sem-and-year functionality added

--- src/controller/routine.controller.js
+++ src/controller/routine.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asyncHandler.js"
 import { Teacher } from "../model/teacher.model.js"
 import { Subject } from "../model/subject.model.js"
 import { Routine } from "../model/routine.model.js"
+import { Department } from "../model/department.model.js"
 
 
 const createRoutine = asyncHandler(async (req, res) => {
@@ -37,7 +38,8 @@ const createRoutine = asyncHandler(async (req, res) => {
         subject_id: subject._id,
         room_no,
         dept_id: req.user.dept_id,
-        sem, year
+        sem: parseInt(sem), 
+        year: parseInt(year)
     })
     if(!routine){
         return res.status(500).json({
@@ -52,6 +54,132 @@ const createRoutine = asyncHandler(async (req, res) => {
     })
 })
 
+const getRoutineByDay = asyncHandler(async (req, res) => {
+    const { day_of_week } = req.body;
+
+    if(day_of_week == ""){
+        return res.status(400).json({
+            "success": false,
+            "message": "all fields are required"
+        })
+    }
+    const routine = await Routine.aggregate([
+        {
+          $match: {
+            "day_of_week":day_of_week,
+            "teacher_id": req.user._id
+          }
+        },
+        {
+          $lookup: {
+            from: "subjects",
+            localField: "subject_id",
+            foreignField: "_id",
+            as: "subject_details"
+          }
+        },
+        {
+          $addFields: {
+            "subject_name": {
+              $first: "$subject_details.name"
+            }
+          }
+        },
+        {
+          $project: {
+            subject_name: 1,
+            time_slot: 1
+          }
+        }
+      ])
+    if(!routine){
+        return res.status(400).json({
+            "success": false,
+            "message": "no routine available"
+        })
+    }
+
+    return res.status(200).json({
+        "success": true,
+        routine
+    })
+})
+
+const getRoutineBySemAndYear = asyncHandler(async (req, res) => {
+    const { dept_name, branch, sem, year } = req.body;
+
+    if(dept_name == "" || branch == "" || sem == "" || year == ""){
+        return res.status(400).json({
+            "success": false,
+            "message": "all fields are required"
+        })
+    }
+
+    const dept = await Department.findOne({ dept_name, branch })
+    if(!dept){
+        return res.status(400).json({
+          "success": false,
+          "message": "department with this name or branch doesn't exist"
+        })
+    }
+
+    const routines = await Routine.aggregate([
+      {
+        $match: {
+          "dept_id": dept._id,
+          "sem": parseInt(sem),
+          "year":parseInt(year)
+        }
+      },
+      {
+        $lookup: {
+          from: "subjects",
+          localField: "subject_id",
+          foreignField: "_id",
+          as: "subject_details"
+        }
+      },
+      {
+        $lookup: {
+          from: "teachers",
+          localField: "teacher_id",
+          foreignField: "_id",
+          as: "teacher_details"
+        }
+      },
+      {
+        $addFields: {
+          "subject_name": {
+            $first: "$subject_details.name"
+          },
+          "teacher_name": {
+            $arrayElemAt: ["$teacher_details.name", 0]
+          }
+        }
+      },
+      {
+        $project: {
+          subject_name: 1,
+          teacher_name: 1,
+          time_slot: 1,
+          day_of_week: 1,
+        }
+      }
+    ])
+    if(routines == []){
+      return res.status(400).json({
+        "success": false,
+        "message": "no routine found"
+      })
+    }
+    return res.status(200).json({
+      "success": false,
+      routines
+    })
+})
+
 export {
-    createRoutine
+    createRoutine,
+    getRoutineByDay,
+    getRoutineBySemAndYear
 }
--- src/router/routine.router.js
+++ src/router/routine.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { verifyJWT, isHOD } from "../middleware/auth.middleware.js"
-import { createRoutine } from "../controller/routine.controller.js"
+import { createRoutine, getRoutineByDay, getRoutineBySemAndYear } from "../controller/routine.controller.js"
 
 
 const router= Router()
 
 router.route("/create").post(verifyJWT, isHOD, createRoutine)
+router.route("/get-routine-by-day").get(verifyJWT, getRoutineByDay)
+router.route("/get-routine-by-sem-and-year").get(getRoutineBySemAndYear)
 
 export default router;
