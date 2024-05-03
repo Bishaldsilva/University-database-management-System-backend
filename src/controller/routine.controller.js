93fe1fb4697db39fb1c689b005baa2c69e77582a
Bishal <bishald288@gmail.com>
2024-05-03 10:04:07 +0530

    update routine functionality done

--- src/controller/routine.controller.js
+++ src/controller/routine.controller.js
@@ -178,8 +178,68 @@ const getRoutineBySemAndYear = asyncHandler(async (req, res) => {
     })
 })
 
+const updateRoutine = asyncHandler(async (req, res) => {
+  const { teacher_code, day_of_week, time_slot, subject_code, room_no, sem, year } = req.body;
+  const { id } = req.params;
+
+  if(teacher_code == "" || day_of_week == "" || time_slot == "" || subject_code == "" || room_no == "" || sem == "" || year == ""){
+      return res.status(400).json({
+          "success": false,
+          "message": "all fields are required"
+      })
+  }
+
+  const teacher = await Teacher.findOne({ code: teacher_code });
+  const subject = await Subject.findOne({ code: subject_code });
+
+  if(!teacher){
+      return res.status(400).json({
+          "success": false,
+          "message": "teacher with this code doesn't exist"
+      })
+  }
+
+  if(!subject){
+      return res.status(400).json({
+          "success": false,
+          "message": "subject with this code doesn't exist"
+      })
+  }
+
+  const routine = await Routine.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+          teacher_id: teacher._id,
+          day_of_week, time_slot,
+          subject_id: subject._id,
+          room_no,
+          dept_id: req.user.dept_id,
+          sem: parseInt(sem), 
+          year: parseInt(year)
+      }
+    },
+    {
+      new: true
+    }
+  )
+
+  if(!routine){
+    return res.status(500).json({
+        "success": false,
+        "message": "something went wrong while updating the routine"
+    })
+  }
+
+  return res.status(200).json({
+      "success": true,
+      routine
+  })
+})
+
 export {
     createRoutine,
     getRoutineByDay,
-    getRoutineBySemAndYear
+    getRoutineBySemAndYear,
+    updateRoutine
 }
--- src/router/routine.router.js
+++ src/router/routine.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { verifyJWT, isHOD } from "../middleware/auth.middleware.js"
-import { createRoutine, getRoutineByDay, getRoutineBySemAndYear } from "../controller/routine.controller.js"
+import { createRoutine, updateRoutine, getRoutineByDay, getRoutineBySemAndYear } from "../controller/routine.controller.js"
 
 
 const router= Router()
 
 router.route("/create").post(verifyJWT, isHOD, createRoutine)
+router.route("/update/:id").post(verifyJWT, isHOD, updateRoutine)
 router.route("/get-routine-by-day").get(verifyJWT, getRoutineByDay)
 router.route("/get-routine-by-sem-and-year").get(getRoutineBySemAndYear)
 
